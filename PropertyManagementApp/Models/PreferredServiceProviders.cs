3fdb852e3882a8fc0400dbb1f1296d2a8a85119c
Jen <jnnfrlocke@gmail.com>
2017-11-03 11:01:23 -0500

    Create TypesOfService and PreferredServiceProviders models

---
+++ PropertyManagementApp/Models/PreferredServiceProviders.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PropertyManagementApp.Models
+{
+    public class PreferredServiceProviders
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Type of Service")]
+        public string Type { get; set; }
+
+        [Display(Name ="Pre-Approved")]
+        public bool PreApproved { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+        public string Contact { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name ="Office Phone Number")]
+        public int OfficePhone { get; set; }
+
+        [Phone]
+        [Display(Name ="Mobile Phone Number")]
+        public int? MobilePhone { get; set; }
+
+        [EmailAddress]
+        [Display(Name ="Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name ="Street Address")]
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [Display(Name ="Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Url]
+        public string Website { get; set; }
+    }
+}
---
+++ PropertyManagementApp/Models/TypesOfService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PropertyManagementApp.Models
+{
+    public class TypesOfService
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Landscaping { get; set; }
+
+        [Display(Name ="Snow Removal")]
+        public string SnowRemoval { get; set; }
+
+        public string Pavement { get; set; }
+
+        public string Exterior { get; set; }
+
+        public string Plumbing { get; set; }
+
+        public string Electrical { get; set; }
+
+        public string Appliances { get; set; }
+
+        public string Painting { get; set; }
+
+        public string General { get; set; }
+
+        public string HVAC { get; set; }
+
+        public string Roofing { get; set; }
+
+        public string Windows { get; set; }
+
+        public string Cleaning { get; set; }
+
+        public string Carpet { get; set; }
+
+        [Display(Name ="Drywall and Insulation")]
+        public string DrywallInsulation { get; set; }
+
+        public string Doors { get; set; }
+
+    }
+}
